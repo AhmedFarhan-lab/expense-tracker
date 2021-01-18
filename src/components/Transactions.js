ea66762020c2a453879cc70088e7ceed5a8d1649
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2021-01-18 14:35:16 +0500

    Created app reducer and binded it to GlobalState, created a consts for fetching expenses and income, created a component that totals the expense and income, created balance component where the total balance is shown, imported useContext and use useContext to take out the needed thing.

---
+++ src/components/Transactions.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+export const Transactions = ({ transaction }) => {
+    const {deleteTransaction} = useContext(GlobalContext)
+    const sign = transaction.amount < 0 ? '-' : '+'
+    return (
+        <li onClick = {() => deleteTransaction(transaction.id)} className={transaction.amount < 0 ? 'minus' : 'plus'}>
+            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className='delete-btn'>x</button>
+        </li>
+    )
+}
---
+++ src/context/GlobalState.js
@@ -0,0 +1,38 @@
+import { createContext, useReducer } from "react"
+import AppReducer from './AppReducer'
+
+const initialState = {
+    transactions: []
+}
+
+export const GlobalContext = createContext(initialState)
+
+export const GlobalProvider =
+    ({ children }) => {
+        const [state, dispatch] =
+            useReducer(AppReducer, initialState)
+
+        function deleteTransaction(id) {
+            dispatch({
+                type: 'DELETE_TRANSACTION',
+                payload: id
+            })
+        }
+        function addTransaction(transaction) {
+            dispatch({
+                type: 'ADD_TRANSACTION',
+                payload: transaction
+            })
+        }
+        return (
+            <GlobalContext.Provider
+                value={{
+                    addTransaction,
+                    transactions: state.transactions,
+                    deleteTransaction
+                }}
+            >
+                {children}
+            </GlobalContext.Provider>
+        )
+    }
